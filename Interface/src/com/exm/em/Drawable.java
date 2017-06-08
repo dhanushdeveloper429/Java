e4532c66f93bcb4b71003098f06c42b56d0fbba4
DHANUSH\DHANUSH <dhanushka9948@gmail.com>
2017-06-08 03:03:35 -0400

    added output

--- Interface/src/com/exm/em/Drawable.java
+++ Interface/src/com/exm/em/Drawable.java
@@ -1,5 +1,8 @@
 package com.exm.em;
 
-public class Drawable {
-
+interface Drawable {
+void draw();
+default void msg(){//we can have method body in interface but we need to make it default method it is available since java8 
+	System.out.println("default method since java 8");
+}
 }
