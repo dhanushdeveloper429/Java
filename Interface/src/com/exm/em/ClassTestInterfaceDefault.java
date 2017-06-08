2f9f05ad167024acf4026d42c53e85cf9dbcd1ca
DHANUSH\DHANUSH <dhanushka9948@gmail.com>
2017-06-08 01:56:15 -0400

    we have method body interface using default method it is avalible in java 8

---
+++ Interface/src/com/exm/em/ClassTestInterfaceDefault.java
@@ -0,0 +1,9 @@
+package com.exm.em;
+
+public class ClassTestInterfaceDefault {
+public static void main(String args[]){
+	Drawable d= new Rectangle();
+	d.draw();
+	d.msg();
+}
+}
---
+++ Interface/src/com/exm/em/Rectangle.java
@@ -0,0 +1,9 @@
+package com.exm.em;
+ class Rectangle implements Drawable {
+
+	 public void draw(){
+	System.out.println("draw rectangle");
+		
+	}
+}
+
